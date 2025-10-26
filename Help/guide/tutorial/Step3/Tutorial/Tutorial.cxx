074313d3f3109f8d30b255da8953f6ff9a872bfe
harrisonhu66 <harrisonhu66@gmail.com>
2025-10-26 14:09:08 +0800

    Exercise 2 - CMAKE Variables

--- Help/guide/tutorial/Step3/Tutorial/Tutorial.cxx
+++ Help/guide/tutorial/Step3/Tutorial/Tutorial.cxx
@@ -1,6 +1,7 @@
 // A simple program that computes the square root of a number
 
 // TODO3: Include <format>
+#include <format>
 
 #include <iostream>
 #include <string>
@@ -11,7 +12,8 @@ int main(int argc, char* argv[])
 {
   if (argc < 2) {
     // TODO4: Convert the print to use std::format
-    std::cout << "Usage: " << argv[0] << " number" << std::endl;
+    std::cout << std::format("Usage: {} number", argv[0]) << std::endl;
+    // std::cout << "Usage: " << argv[0] << " number" << std::endl;
     return 1;
   }
 
@@ -21,6 +23,7 @@ int main(int argc, char* argv[])
   // calculate square root
   double const outputValue = mathfunctions::sqrt(inputValue);
   // TODO5: Convert the print to use std::format
-  std::cout << "The square root of " << inputValue << " is " << outputValue
-            << std::endl;
+  // std::cout << "The square root of " << inputValue << " is " << outputValue
+  //           << std::endl;
+  std::cout << std::format("The square root of {} is {}", inputValue, outputValue) << std::endl;
 }
